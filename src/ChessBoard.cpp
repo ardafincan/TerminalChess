16c20b5c7d62c66d7c72f6a45a321e54851be3ea
Ali Arda Fincan <ardafincan@icloud.com>
2024-12-31 01:24:37 +0300

    last commit was incomplete due to unsaved changes, also updated .gitignore

--- src/ChessBoard.cpp
+++ src/ChessBoard.cpp
@@ -21,16 +21,23 @@
     bool ChessBoard::movePiece(Position source, Position target){
         MoveValidator validator(this);
         
-        if(!validator.isMoveValid(source, target)){
+        if(!validator.isMoveValid(source, target) || !validator.isPathValid(source, target)){
             return false;
         }
-
         if(!board.at(source).hasMoved){
             board.at(source).hasMoved = true;
         }
         //implement here if condition checking if source exists and target position is within chess board
-            if(!board.contains(target)){
-                board.insert(std::make_pair(source, board.at(source)));
-                board.erase(source);
-            }
+        if(!board.contains(target)){
+            board.insert(std::make_pair(source, board.at(source)));
+            board.erase(source);
+        }else if(board.at(target).color == board.at(source).color){
+            return false;
+        }else{
+            ChessBoard::capturePiece(source, target);
+        }
+    };
+
+    bool ChessBoard::capturePiece(Position source, Position target){
+        
     };
